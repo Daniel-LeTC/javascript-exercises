41faf8e89ff28ab25d67aa7d2db96121875c484b
DanielAtOffice <117688408+Daniel-LeTC@users.noreply.github.com>
2023-06-01 15:13:14 +0700

    lưu ý add return để hoàn giá trị cho function, nếu không có sẽ bị undefined

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(inPut,num) {
+    
+    let string="";
+    for (let i = 1; i<=num; i++) {
+        
+        string += inPut; 
+    }   
+   return string;     
 
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
