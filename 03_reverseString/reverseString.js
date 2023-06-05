ddc553c350d8a3494b170a19d568b616c1301614
DanielAtOffice <117688408+Daniel-LeTC@users.noreply.github.com>
2023-06-05 14:18:28 +0700

    Add the function, var, and comment for each line to reverse a string, please note that we can use many method at the same line, for ex: string.split().reverse().join() => No need to seperate these methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let myArr = word.split(""); //split the string into array for easiser order arrangement by using the split seperator with an empty string ""
+    let myReverseArr = myArr.reverse(); //reverse the original array
+    let mirror = myReverseArr.join(""); // Join the reversed array by using the Join Method with the seperator is an empty string ""
+    
+    return mirror;
+    
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
