04562e61b47223ec997c129ca22af96c231da640
DanielAtOffice <117688408+Daniel-LeTC@users.noreply.github.com>
2023-06-27 16:09:30 +0700

    Update comment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
    
-    if (year % 4 === 0 && year % 400 === 0 || year % 4 === 0 && year % 100 !== 0  ) {
+    if (year % 4 === 0 && year % 400 === 0 || year % 4 === 0 && year % 100 !== 0  ) {   //return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0); we can use return directly to output boolean, and we can group &&, || just like any other operators
         return true;
     } else return false;
    
