081aa8cae047399b7d8a649b304a09915cc5fb23
DanielAtOffice <117688408+Daniel-LeTC@users.noreply.github.com>
2023-06-28 10:07:13 +0700

    Update comment && finished exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  let tempCelcius = (fahrenheit-32)*5/9;
+  return Number(tempCelcius.toFixed(1));  //toFixed() methods round number to decided decimal but it convert the number into string, hence we should use number() function.
 };
 
-const convertToFahrenheit = function() {
+//console.log(convertToCelsius(125));
+
+const convertToFahrenheit = function(celcius) {
+  
+  let tempFahrenheit = celcius * 9 / 5 + 32;
+  return Number(tempFahrenheit.toFixed(1));
 };
+
+//console.log(convertToFahrenheit(51.67));
 
 // Do not edit below this line
-module.exports = {
+
+  module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
