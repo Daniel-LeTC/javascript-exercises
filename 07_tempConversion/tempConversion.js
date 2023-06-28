cb8022376602a96acb2989b70efdb867ea404462
DanielAtOffice <117688408+Daniel-LeTC@users.noreply.github.com>
2023-06-28 14:38:54 +0700

    Update comment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -9,8 +9,8 @@ const convertToCelsius = function(fahrenheit) {
 const convertToFahrenheit = function(celcius) {
   
   let tempFahrenheit = celcius * 9 / 5 + 32;
-  return Number(tempFahrenheit.toFixed(1));
-};
+  return Number(tempFahrenheit.toFixed(1)); // Another option is that we can use math.round(number * 10) / 10, because math.round will output integer only so we time 10 and devide to 10 in order to return the 1 decimal format
+};                                          // For ex: number is 10.3456, we want 10.3. When we applied above the output will be 103 for math.round, then devide to 10 we got 10.3 as what we wanted.
 
 //console.log(convertToFahrenheit(51.67));
 
