58019fbb36dc532dec562755d9bfd82d1cfa6a90
DanielAtOffice <117688408+Daniel-LeTC@users.noreply.github.com>
2023-06-28 14:38:43 +0700

    Update comment

--- 07_tempConversion/solution/tempConversion-solution.js
+++ 07_tempConversion/solution/tempConversion-solution.js
@@ -2,9 +2,13 @@ const convertToCelsius = function (fahrenheit) {
   return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
+console.log (convertToCelsius(125));
+
 const convertToFahrenheit = function (celsius) {
   return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
+
+console.log (convertToFahrenheit(51.7));
 
 module.exports = {
   convertToCelsius,
